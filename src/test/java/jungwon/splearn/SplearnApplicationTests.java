fcdab625f6dd385c2c852bc1e4fffdedbe737ad9
Jungwon Kim <mac2001035@gmail.com>
2026-02-19 16:46:07 +0900

    INIT: 프로젝트 시작

---
+++ src/test/java/jungwon/splearn/SplearnApplicationTests.java
@@ -0,0 +1,13 @@
+package jungwon.splearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
